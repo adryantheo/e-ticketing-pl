8db6495f665346f3963bf33c25ff4ed60f77f899
Adryan Theo <adryantheo@gmail.com>
2019-08-16 14:10:16 +0700

    Tutor Backend

---
+++ app/Http/Controllers/TicketController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Ticket;
+use Illuminate\Http\Request;
+
+class TicketController extends Controller
+{
+    
+    public function index()
+    {
+        return response()->json(Ticket::all(), 200);
+    }
+
+    
+    public function store(Request $request)
+    {
+        $ticket = Ticket::create([
+            'harga' => $request->input('harga')
+        ]);
+
+        return response()->json($ticket, 201);
+    }
+
+    
+    public function show(Ticket $ticket)
+    {
+        return response()->json($ticket, 200);
+    }
+
+    
+    public function update(Request $request, Ticket $ticket)
+    {
+        //
+    }
+
+    public function destroy(Ticket $ticket)
+    {
+        $status = $ticket -> delete();
+
+        return response()->json([
+            'status' => $status,
+            'message' => $status ? "Berhasil Menghapus Ticket" : "Gagal Menghapus Ticket"
+        ]);
+    }
+}
