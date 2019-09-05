bbc85deeac2e2072c8b05b2b1bb6106e224aee99
hamonangan silalahi <yohaneshamonangan@ndamelweb.id>
2019-09-05 10:18:31 +0700

    user authentication

--- resources/js/helper/AppStorage.js
+++ resources/js/helper/AppStorage.js
@@ -1,7 +1,24 @@
 class AppStorage
 {
     storeToken(token){
-        localStorage.setItem('ticketinf-token', token)
+        localStorage.setItem('ticketing-token', token)
     }
-    
+    storeUser(user){
+        localStorage.setItem('ticketing-user', user)
+    }
+
+    store(token, user){
+        this.storeUser(token);
+        this.storeToken(user);
+    }
+
+    clear(){
+        localStorage.removeItem('ticketing-token')
+        localStorage.removeItem('ticketing-user')
+    }
+
+    getToken(){
+        return localStorage.getItem('ticketing-token')
+    }
 }
+export default AppStorage = new AppStorage();
