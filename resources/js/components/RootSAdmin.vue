c8923fc373500dba067d0a153fa629d8f6bb212f
A.Theo <adryantheo@gmail.com>
2019-09-13 23:19:50 +0700

    Router Push Based on User's Privileges for All Pages

--- resources/js/components/RootSAdmin.vue
+++ resources/js/components/RootSAdmin.vue
@@ -3,6 +3,28 @@
 </template>
 <script>
 export default {
+   data: () => ({
+      Role: undefined,
+    }),
+
+    methods:{
+        isPrivileges(){
+            this.Role = localStorage.getItem('Role');
+            //Role IDnya ikutin Sesuai DB
+            if(this.Role == 1){
+               this.$router.replace({path: "/superadmin"}); //untuk super admin
+            }
+            else if(this.Role == 3){
+               this.$router.replace({path: "/admin"}); //untuk admin
+            }
+            else{
+               this.$router.replace({path: "/"}); //untuk user
+            }
+        }
+    },
+    mounted(){
+        this.isPrivileges();
+    }
     
 }
 </script>
