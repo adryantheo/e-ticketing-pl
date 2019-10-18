b67aefb796a212a2a539d1cd7f3d326c32c4c5fc
Adryan Theo <adryantheo@gmail.com>
2019-10-18 15:53:13 +0700

    Fix Return Products in TicketCust

--- resources/js/components/customers/pages/TicketCust.vue
+++ resources/js/components/customers/pages/TicketCust.vue
@@ -12,29 +12,29 @@
         </v-layout>
         <template v-for="(item, i) in productTicket"> 
             <v-layout :key="`am-${i}`">
-                <v-flex v-if="!!product.length > 0" style="max-height:100px">
+                <v-flex v-if="cekQR > 0" style="max-height:100px">
                     <v-card>
                         <v-flex>
                             <v-card-text>
                                 <qr-code :text="item.qr_codes.qr_code"></qr-code>
-                                <h1>aaaaa</h1>
+                                <h1></h1>
                             </v-card-text>
                         </v-flex>
                     </v-card>
                 </v-flex>
-                <v-flex v-else xs12 md9 >
-                    <v-card class="rounded" elevation="10">
+            </v-layout>
+        </template>
+        <v-flex v-if="cekQR != 1" xs12 md9 >
+                    <v-card v-if="!loading" class="rounded" elevation="10">
                         <v-card-text class="text-xs-center font-weight-bold"> 
                             Anda Belum Memiliki Tiket <br> Silahkan Membeli Tiket dengan Menekan Tombol dibawah
                             <v-spacer></v-spacer>
                             <v-btn outline round color="info">
-                                Pesan TIket
-                            </v-btn>
-                        </v-card-text>
-                    </v-card>
-                </v-flex>
-            </v-layout>
-        </template>
+                            Pesan TIket
+                        </v-btn>
+                    </v-card-text>
+                </v-card>
+            </v-flex>
     </v-container>
 </template>
 <script>
@@ -43,7 +43,7 @@ export default {
     data: () => ({
         loading:false,
         productTicket: [],
-        product: ""
+        cekQR: 0,
     }),
     methods: {
         fetchAllTicket(){
@@ -54,9 +54,11 @@ export default {
             this.loading = true
             try{
                 const res = await this.fetchAllTicket()
-                this.productTicket = res.data
-                this.product = this.productTicket
-                console.log(this.product)
+                this.productTicket = res.data;
+                if(this.productTicket.length > 0){
+                    this.cekQR = 1;
+                }
+                console.log(this.productTicket.length)
             }catch(err){
                 console.log(err)
             }
